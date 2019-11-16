6ec77ade1ead8d78d798a48752046c6936762561
lalithadonga <lalitha.donga21@gmail.com>
2019-11-15 22:22:14 -0800

    Learning state through an onClick dismiss button

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+]; 
+
+// pass a function to map 
+const array = [1, 4, 9, 16]; 
+const newArray = array.map(function(x) {return x * 2}); 
+console.log(newArray); 
+// expected output: Array [2, 6, 18, 32]
+
+class App extends Component {
+  
+  constructor(props) { 
+    super(props);
+
+    this.state = {
+      list, // instead of list: list 
+    };
+
+    this.onDismiss = this.onDismiss.bind(this);
+  } 
+
+
+
+  onDismiss = (id) => {
+    const isNotId = item => item.objectID !== id;
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({ list: updatedList }) // update the list in the local component state
+  }
+
+  render() { 
+    return (
+      <div className="App">
+        {this.state.list.map(item => 
+          <div key ={item}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span> {item.author} </span>
+            <span> {item.num_comments} </span>
+            <span> {item.points} </span>
+            <span> 
+              <button 
+                onClick={() => this.onDismiss(item.objectID)}
+                type = "button"
+              > 
+                Dismiss
+              </button>
+            </span>
+          </div>
+        )}
+      </div>
+    );
+  };
 }
 
 export default App;
